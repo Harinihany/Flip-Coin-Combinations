31f639c3dc0e118135670cf819877a5ba613f7b3
Harinihany <harinihany03@gmail.com>
2020-11-22 13:14:47 +0530

    added:doublet logic

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,23 +1,19 @@
 #! /bin/bash
 
-headCount=0
-tailCount=0
-declare -A singletDictionary
-read -p "Enter the times of flip " num
+echo -e "1: For Singlet \n2: For Doublet"
+declare -A combinationDictionary
+
+function singlet()
+{
+num=$1
 for ((counter=1; counter <= $num; counter++))
 do
 	flip=$((RANDOM%2))
-	if (( $flip == 1 ))
-	then
-		headCount=$((headCount+1))
-		singletDictionary[1]=$headCount
-	else
-		tailCount=$((tailCount+1))
-		singletDictionary[2]=$tailCount
-	fi
+	combinationDictionary[$flip]=$((${combinationDictionary[$flip]} +1))	
+
 done
-	singHeadProb=$((${singletDictionary[1]} *100 / $num ))
-	singTailProb=$((${singletDictionary[2]} *100 / $num ))
+singHeadProb=$(( (${combinationDictionary[0]}*100) / $num ))
+singTailProb=$(( (${combinationDictionary[1]}*100) / $num ))
 
 
 if (( $singHeadProb > $singTailProb ))
@@ -29,4 +25,54 @@ else
 	echo "Tail = $singTailProb %"
 fi
 
-echo "Head: ${singletDictionary[1]} Tail: ${singletDictionary[2]}"
+echo "Head: ${combinationDictionary[0]} Tail: ${singletDictionary[1]}"
+
+}
+
+function Doublet()
+{
+ num=$1
+         for ((counter=1; counter <= $num; counter++))
+        do
+                flip=$((RANDOM%4))
+                combinationDictionary[$flip]=$((${combinationDictionary[$flip]} +1))
+        done
+        SingHHProb=$(( (${combinationDictionary[0]}*100) / $num ))
+        SingTTProb=$(( (${combinationDictionary[1]}*100) / $num ))
+        SingHTProb=$(( (${combinationDictionary[2]}*100) / $num ))
+        SingTHProb=$(( (${combinationDictionary[3]}*100) / $num ))
+
+      
+        if (( $SingHHProb >= $SingTTProb && $SingHHProb >= $SingTHProb && $SingHHProb >= $SingHTProb ))
+        then
+                echo "$SingHHProb %"
+        elif (( $SingTTProb >= $SingHHProb && $SingTTProb >= $SingTHProb && $SingTTProb >= $SingHTProb ))
+        then
+                echo "TT = $SingTTProb %"
+        elif (( $SingTHProb >= $SingTTProb && $SingTHProb >= $SingHHProb && $SingTHProb >= $SingHTProb ))
+        then
+                echo "TH = $SignTHProb %"
+        else
+                echo "HT = $SignHTProb %"
+
+        fi
+
+        echo "HH: ${combinationDictionary[0]} TT: ${combinationDictionary[1]}  HT : ${combinationDictionary[2]}   TH : ${combinationDictionary[3]}"
+
+}
+
+read -p "Enter the times of flip " num
+read -p "Select the option: " option
+
+case "$option" in
+1)
+
+	singlet $num
+	;;
+2)
+	Doublet $num
+	;;
+*)
+	echo "0"
+	;;
+esac
